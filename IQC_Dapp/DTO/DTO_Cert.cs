150fccd3deb714153c94b8cfac4abb8060097fcc
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2019-04-02 18:50:14 +0200

    Added IQC Block Cert

---
+++ IQC_Dapp/DTO/DTO_Cert.cs
@@ -0,0 +1,32 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace IQC_Dapp.DTO
+{
+    //function getCert(address index) public view returns(
+    //string memory issuedBy, 
+    //string memory issuedTo,
+    //string memory issuingDate,
+    //string memory certHash) 
+    [FunctionOutput]
+    public class DTO_Cert
+    {
+        [Parameter("bytes32", "issuedBy", 1)]
+        [Display(Name = "Issued By")]
+        public String IssuedBy { get; set; }
+        [Parameter("bytes32", "issuedTo", 2)]
+        [Display(Name = "Issued To")]
+        public String IssuedTo { get; set; }
+        [Parameter("bytes32", "issuingDate", 3)]
+        [Display(Name = "Date of Issue")]
+        public String IssuingDate { get; set; }
+        [Parameter("bytes32", "certHash", 4)]
+        [Display(Name = "Cert Hash")]
+        public String CertHash { get; set; }
+    }
+}
